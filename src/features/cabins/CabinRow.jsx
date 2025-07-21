94267b2b40d1209b074b8767d052cb3ef5d3a870
Beridze0 <beridzedavit904@gmail.com>
2025-07-21 07:27:57 +0400

    Displaying Toasts (Notifications)

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
 
 const TableRow = styled.div`
   display: grid;
@@ -62,7 +63,7 @@ export default function CabinRow({ cabin }) {
         queryKey: ["cabins"],
       });
     },
-    onError: (err) => alert(err.message),
+    onError: (err) => toast.error(err.message),
   });
 
   return (

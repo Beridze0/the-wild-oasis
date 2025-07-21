aebbe13059cc87985b92711c609e5c75d777e69b
Beridze0 <beridzedavit904@gmail.com>
2025-07-21 09:50:36 +0400

    Uploading Images to Supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://jjbjixvfcnbhjiuvinuw.supabase.co";
+export const supabaseUrl = "https://jjbjixvfcnbhjiuvinuw.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpqYmppeHZmY25iaGppdXZpbnV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI3ODM4MzAsImV4cCI6MjA2ODM1OTgzMH0.x_GNNLT5SEnw1sim5_VoBVzy_mrIVtztjUJflc7OnTc";
 const supabase = createClient(supabaseUrl, supabaseKey);

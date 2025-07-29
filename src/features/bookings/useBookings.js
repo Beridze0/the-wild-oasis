246fcc8bafb30f470698d38e7cc632b18d94515e
Beridze0 <beridzedavit904@gmail.com>
2025-07-29 19:32:36 +0400

    API-Side Pagination: Paginating Bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -14,16 +14,19 @@ export function useBookings() {
 
   const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
 
+  // PAGINATION
+  const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
+
   const [field, direction] = sortByRaw.split("-");
   const sortBy = { field, direction };
 
   const {
     isLoading,
-    data: bookings,
+    data: { data: bookings, count } = {},
     error,
   } = useQuery({
-    queryKey: ["bookings", filter, sortBy],
-    queryFn: () => getBookings({ filter, sortBy }),
+    queryKey: ["bookings", filter, sortBy, page],
+    queryFn: () => getBookings({ filter, sortBy, page }),
   });
-  return { isLoading, bookings, error };
+  return { isLoading, bookings, error, count };
 }
